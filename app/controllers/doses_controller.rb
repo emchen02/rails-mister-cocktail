75f60c04034e1c91d958ac071648c0a0df714482
Emily Chen <emchen02@gmail.com>
2020-11-26 17:11:14 +0800

    done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,27 +2,30 @@ class DosesController < ApplicationController
   def new
     # create a new instance and pass it to new html
     @dose = Dose.new
-    # find the cocktail to update
+    # find the cocktail to update the dose to
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
-    # pass the params from the form over to create the new Dose
+    # pass the params from the new form over to create the new Dose
     @dose = Dose.new(params_dose)
     # find the cocktail to create dose for
     @cocktail = Cocktail.find(params[:cocktail_id])
     # assign the new dose created to the correct cocktail
     @dose.cocktail = @cocktail
     if @dose.save
+      # if saved, go to the cocktail/:id url
       redirect_to cocktail_path(@cocktail)
     else
+      # render new goes to the new.html.erb file form
       render :new
     end
   end
 
   def destroy
-    # find the cocktail to destroy the dose
+    # find the dose to destroy
     @dose = Dose.find(params[:id])
+    # find the cocktail of that dose
     @cocktail = @dose.cocktail
     @dose.destroy
     redirect_to cocktail_path(@cocktail.id)
