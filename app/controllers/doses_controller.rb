33f3395b0e95009fd4dbd40e5708ba77eb26860f
Emily Chen <emchen02@gmail.com>
2020-11-26 16:38:49 +0800

    done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,9 +7,11 @@ class DosesController < ApplicationController
   end
 
   def create
+    # pass the params from the form over to create the new Dose
     @dose = Dose.new(params_dose)
+    # find the cocktail to create dose for
     @cocktail = Cocktail.find(params[:cocktail_id])
-    # assign it to the correct cocktail
+    # assign the new dose created to the correct cocktail
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
