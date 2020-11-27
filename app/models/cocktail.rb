3f59c977d7fabdd4e9993808679b06fdf1c941f2
Emily Chen <emchen02@gmail.com>
2020-11-27 12:03:42 +0800

    done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
-  has_many :doses, dependent: :destroy
-  has_many :ingredients, through: :doses
+  has_many :doses, dependent: :destroy #=> def doses
+  has_many :ingredients, through: :doses #=> def ingredients
   validates :name, presence: true, uniqueness: true
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktail
-  belongs_to :ingredient
+  belongs_to :cocktail  #=> def cocktails
+  belongs_to :ingredient  #=> def ingredient
   validates :description, presence: true
   validates :ingredient, presence: true
   validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
